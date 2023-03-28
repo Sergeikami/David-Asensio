f898f966c1ac5ee66208b2f1b98309ed81fe70eb
Sergeikami <127503226+Sergeikami@users.noreply.github.com>
2023-03-28 09:52:18 +0200

    Initial commit

---
+++ Main-body.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+var velocitat := 50000
+var moviment := Vector2(0,0)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.action_press('ui_right'):
+		moviment = Vector2.RIGHT * velocitat
