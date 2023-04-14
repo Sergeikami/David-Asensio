b548857f83c9f178ac36918f5beb3798e1b2da66
Sergeikami <127503226+Sergeikami@users.noreply.github.com>
2023-04-14 13:15:56 +0200

    Personatge principal amb moviment

--- Main-body.gd
+++ Main-body.gd
@@ -1,17 +1,35 @@
 extends KinematicBody2D
-
+var velocitat := 50
 
 # Declare member variables here. Examples:
-var velocitat := 50000
-var moviment := Vector2.ZERO
+# var a = 2
+# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	position = Vector2(500,300)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.action_press('ui_right'):
-		moviment = Vector2.RIGHT * velocitat
+	if Input.is_action_pressed("ves_dreta"):
+		if Input.is_action_pressed("ves_dreta") and Input.is_action_pressed("ves_amunt"):
+			position += Vector2(0.707,-0.707) * velocitat * delta
+		elif Input.is_action_pressed("ves_dreta") and Input.is_action_pressed("ves_abaix"):
+			position += Vector2(0.707,0.707) * velocitat * delta
+		else:
+			position += Vector2(1,0) * velocitat * delta
+	elif Input.is_action_pressed("ves_esquerra"):
+		if Input.is_action_pressed("ves_esquerra") and Input.is_action_pressed("ves_amunt"):
+			position += Vector2(-0.707,-0.707) * velocitat * delta
+		elif Input.is_action_pressed("ves_esquerra") and Input.is_action_pressed("ves_abaix"):
+			position += Vector2(-0.707,0.707) * velocitat * delta
+		else:
+			position += Vector2(-1,0) * velocitat * delta
+	elif Input.is_action_pressed("ves_amunt"):
+		position += Vector2(0,-1) * velocitat * delta
+	elif Input.is_action_pressed("ves_abaix"):
+		position += Vector2(0,1) * velocitat * delta
+	
+	
